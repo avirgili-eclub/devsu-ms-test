f27ad4ac329d57532efcc68989f2a74793becc37
avirgili-eclub <avirgili-eclub@users.noreply.github.com>
2026-05-03 02:19:41 -0300

    initial commit

---
+++ src/test/java/com/devsu/msone/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.msone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
